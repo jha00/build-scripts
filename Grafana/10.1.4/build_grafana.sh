0854fadc942e83c32c98195fc4f162bfd41d9ccb
Sankalp <sankalpjha225@gmail.com>
2023-12-12 18:19:05 +0530

    Update build_grafana.sh

--- Grafana/10.1.4/build_grafana.sh
+++ Grafana/10.1.4/build_grafana.sh
@@ -3,7 +3,7 @@
 # Package	: Grafana
 # Version	: v10.1.4
 # Source repo	: https://github.com/grafana/grafana
-# Tested on	: Ub20.04
+# Tested on	: rh7.8, rh7.9, rh8.6, rh8.8, rh9.0, rh9.2, sl12.5, sl15.4, sl15.5, ub20.04, ub22.04, ub23.04
 # Language      : Typescript
 # Travis-Check  : False
 # Script License: Apache License, Version 2 or later
